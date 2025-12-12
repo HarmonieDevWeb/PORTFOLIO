a935e5313a87e3f8e911a699fd1f2c98fc6090e6
HarmonieDevWeb <chevrel.h.pro@gmail.com>
2025-12-12 10:42:00 +0100

    Add Admin Page

---
+++ my-portofilo/app/admin/page.js
@@ -0,0 +1,32 @@
+"use client";
+import "../globals.css";
+
+
+export default function Home() {
+  return (
+    <div id="home" className="min-h-screen w-full">
+
+      {/* MAIN */}
+      <main className="w-full bg-background py-4">
+
+        <div className="max-w-5xl mx-auto px-10 py-4">
+
+          {/* ADMIN SECTION */}
+        
+
+
+
+        </div>
+      </main>
+
+      {/* ARTICLE SECTION  */}
+      <div className=" bg-white w-full py-16 px-4">
+
+
+
+      </div>
+
+
+    </div>
+  );
+}
