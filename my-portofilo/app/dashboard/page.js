b7b1b2a11c65ece5891dccf57d7fc05a77670c3b
HarmonieDevWeb <chevrel.h.pro@gmail.com>
2025-12-15 16:26:14 +0100

    Feat add AdimDash Page Appel API CRUD

--- my-portofilo/app/dashboard/page.js
+++ my-portofilo/app/dashboard/page.js
@@ -1,10 +1,55 @@
 "use client";
-
-import React from 'react';
 import { useState } from 'react';
-import { Lock, User, Plus, Edit2, Trash2, Save, X, Eye, EyeOff, LogOut, Home, Briefcase, Code, Mail, FileText } from 'lucide-react';
 
-const AdminDashboard = () => {
-
+export default function AdminDash(){
+    const [projects, setProjects] = useState([]); // les données
+    const [skills, setSkills] = useState([]);
+    const [about, setAbout] = useState({});
+    const [loading, setLoading] = useState(true); // le chargement
+    const [error, setError] = useState(false); // les erreurs
 }
-export default AdminDashboard;
+
+
+    // CRUD OPERATIONS
+
+        //Projects
+    const fetchProjects = async () => {
+        try {
+            // chargement et reset erreurs
+            setLoading(true);
+            setError(null);
+
+            // API Next
+            const res = await fetch("/api/projects");
+
+            // Controle de la requete
+            if (!res.ok) {
+                throw new Error("Erreur lors du chargement des projets.");
+            }
+
+            // Conversion Json
+            const data = await res.json();
+
+            // MAJ affichage projets
+            setProjects(data.projects || []);
+        } catch (err) {
+            console.error("erreur:", err);
+            setError(err.message);
+        } finally {
+            setLoading(false); // Annulation du chargement que ce soit succes ou error
+        }
+    };
+            //Skills
+
+            //About
+
+
+  // Authentification avec Argon2
+
+
+
+  
+
+
+
+
