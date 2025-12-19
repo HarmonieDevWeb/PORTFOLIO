a8960ddd080f99e8195b8dec2bc29d759f47513f
HarmonieDevWeb <chevrel.h.pro@gmail.com>
2025-12-19 16:17:22 +0100

    Feat onclick add diploma hidden true for Formation in coming

--- my-portofilo/components/DashAbout.js
+++ my-portofilo/components/DashAbout.js
@@ -1,16 +1,17 @@
-// components/DashAbout.js
 "use client";
+
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { fr } from 'date-fns/locale';
-import { CalendarDays } from "lucide-react";
+import { CalendarDays, CirclePlus, CircleMinus, UserRoundPen } from "lucide-react";
 
 export default function DashAbout() {
   const [formationStartDate, setFormationStartDate] = useState(null);
   const [formationEndDate, setFormationEndDate] = useState(null);
   const [experienceStartDate, setExperienceStartDate] = useState(null);
   const [experienceEndDate, setExperienceEndDate] = useState(null);
+  const isDIploma = useSate(false);
 
   const CustomInput = ({ value, onClick }) => (
     <button
@@ -23,6 +24,13 @@ export default function DashAbout() {
       <CalendarDays className="w-5 h-5 text-gray-400 shrink-0" />
     </button>
   );
+
+  const AddDiploma = ({ onClick }) => (
+    < CirclePlus
+      onClick={onClick}
+    />
+
+  )
 
   return (
     <section className="max-w-4xl mx-auto px-6 py-8 space-y-10">
@@ -47,50 +55,55 @@ export default function DashAbout() {
       </div>
 
       {/* Formations */}
-      <div className="space-y-4">
+      <div className="space-y-6">
         <h2 className="text-xl font-semibold text-gray-800 mb-4">
           Formations
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 border-b-2 border-r-2 border-secondary rounded-2xl">
+        <div onClick className="grid grid-cols-1 md:grid-cols-2 gap-6 border-b-2 border-r-2 border-secondary rounded-2xl">
           <h3>Parcours Académique</h3>
-          <div className="space-y-2">
-          <input type="text" placeholder="Type et nom du diplôme" className="mb-2 bg-white shadow-sm rounded-lg px-2 py-2 border border-gray-200"></input>
-          <input type="text" placeholder="Lieu d'obtention" className="bg-white shadow-sm rounded-lg px-2 py-2 border border-gray-200"></input>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 pl-4">
+          {isDiploma && (
+            <div className="hidden space-y-2"></div>
+          )}
+          {!isDiploma && (
+            <>
             <div className="space-y-2">
-              <label className="block text-sm font-medium text-primary mb-2">
-                Date de début
-              </label>
-              <DatePicker
-                selected={formationStartDate}
-                onChange={(date) => setFormationStartDate(date)}
-                locale={fr}
-                dateFormat="MM/yyyy"
-                customInput={<CustomInput />}
-              />
-            </div>
-            <div className="space-y-2 mb-2">
-              <label className="block text-sm font-medium text-primary mb-2">
-                Date de fin
-              </label>
-              <DatePicker
-                selected={formationEndDate}
-                onChange={(date) => setFormationEndDate(date)}
-                locale={fr}
-                dateFormat="MM/yyyy"
-                customInput={<CustomInput />}
-                minDate={formationStartDate}
-              />
-            </div>
-          </div>
-        </div>
+              <input type="text" placeholder="Type et nom du diplôme" className="mb-2 bg-white shadow-sm rounded-lg px-2 py-2 border border-gray-200"></input>
+              <input type="text" placeholder="Lieu d'obtention" className="bg-white shadow-sm rounded-lg px-2 py-2 border border-gray-200"></input>
+            </div><div className="grid grid-cols-1 md:grid-cols-2 gap-6 pl-4">
+                <div className="space-y-2">
+                  <label className="block text-sm font-medium text-primary mb-2">
+                    Date de début
+                  </label>
+                  <DatePicker
+                    selected={formationStartDate}
+                    onChange={(date) => setFormationStartDate(date)}
+                    locale={fr}
+                    dateFormat="MM/yyyy"
+                    customInput={<CustomInput />} />
+                </div>
+                <div className="space-y-2 mb-2">
+                  <label className="block text-sm font-medium text-primary mb-2">
+                    Date de fin
+                  </label>
+                  <DatePicker
+                    selected={formationEndDate}
+                    onChange={(date) => setFormationEndDate(date)}
+                    locale={fr}
+                    dateFormat="MM/yyyy"
+                    customInput={<CustomInput />}
+                    minDate={formationStartDate} />
+                </div>
+              </div>
+              </>
+          )}
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 border-b-2 border-r-2 border-secondary rounded-2xl">
+          </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 border-b-2 border-r-2 border-secondary rounded-2xl">
           <h3>Certifications</h3>
           <div className="space-y-2">
-          <input type="text" placeholder="Type et nom du diplôme" className="mb-2 bg-white shadow-sm rounded-lg px-2 py-2 border border-gray-200"></input>
-          <input type="text" placeholder="Lieu d'obtention" className="bg-white shadow-sm rounded-lg px-2 py-2 border border-gray-200"></input>
+            <input type="text" placeholder="Type et nom du diplôme" className="mb-2 bg-white shadow-sm rounded-lg px-2 py-2 border border-gray-200"></input>
+            <input type="text" placeholder="Lieu d'obtention" className="bg-white shadow-sm rounded-lg px-2 py-2 border border-gray-200"></input>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 pl-4">
             <div className="space-y-2">
@@ -121,7 +134,6 @@ export default function DashAbout() {
           </div>
         </div>
 
-       
       </div>
 
       {/* Expérience Pro */}
